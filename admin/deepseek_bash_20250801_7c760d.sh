24314bdfd408642d6cfdea7d93c3d0e535da6b1e
carlvddecken <carlvddecken@gmail.com>
2025-08-01 16:41:46 +0200

    Rename deepseek_bash_20250801_7c760d.sh to netlify-cms.js

--- admin/deepseek_bash_20250801_7c760d.sh
+++
@@ -1,1 +0,0 @@
-curl -o admin/netlify-cms.js https://cdn.jsdelivr.net/npm/netlify-cms@^3.0.0/dist/netlify-cms.js
---
+++ admin/netlify-cms.js
@@ -0,0 +1,1 @@
+curl -o admin/netlify-cms.js https://cdn.jsdelivr.net/npm/netlify-cms@^3.0.0/dist/netlify-cms.js
